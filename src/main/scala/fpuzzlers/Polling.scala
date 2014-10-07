8a384bb07ea8c5d1bc2e4e38b5c11d805e69792c
Frode Nerbråten <frode@nerbraten.no>
2014-10-07 08:39:24 +0200

    Flere kommentarer

--- src/main/scala/fpuzzlers/Polling.scala
+++ src/main/scala/fpuzzlers/Polling.scala
@@ -13,6 +13,12 @@ object Polling {
 
   object ImperativePolling {
 
+    /**
+     * Denne klassen kjøres som en egen tråd og poller en ekstern
+     * JVM for å overvåke cpu-load på den eksterne serveren.
+     * Den poller etter cpu-bruk via jmx med 500ms intervaller.
+     * Når den får signal om å slutt returneres resultatene som en liste.
+     */
     class JmxCpuPoller(jmxService: JmxService) extends Runnable {
       var running = true
 
@@ -36,12 +42,19 @@ object Polling {
 
   object FunctionalPolling {
 
+    /**
+     * Denne hjelpefunksjonen returner en uendelig strøm av cpu-resultateter fra en ekstern JVM
+     */
     private def pollJmxCpu(jmxService: JmxService): Stream[Long] =
       Try(jmxService.pollCpu()) match {
         case Success(l) => l #:: pollJmxCpu(jmxService)
         case Failure(e) => pollJmxCpu(jmxService)
       }
 
+    /**
+     * Denne funksjonen brukes til å polle en ekstern JVM for cpu-bruk med 500ms intervaller.
+     * Resultatet er en liste som returneres asynkront via en future.
+     */
     def getJmxCpuStats(jmxService: JmxService, running: () => Boolean): Future[List[Long]] =
       Future {
         pollJmxCpu(jmxService)
